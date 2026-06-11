616fbcba47cadffcba9950b64a48bc978b1ba24b
Connor Ostrander <costrander6@gatech.edu>
2026-06-11 19:10:08 -0400

    feat: transform semgrep output into JSON object for http request

--- scripts/export-results.py
+++ scripts/export-results.py
@@ -36,9 +36,7 @@ def get_actionlint_results(timestamp: datetime) -> ResultsRequest | None:
     
     results_request = ResultsRequest.model_validate(data)
     results_request.timestamp = timestamp
-
-    source = get_repo_info()
-    results_request.source = source
+    results_request.source = get_repo_info()
 
     return results_request
 
@@ -54,9 +52,8 @@ def get_poutine_results(timestamp: datetime) -> ResultsRequest | None:
     
     results_request = ResultsRequest(scanner="poutine", timestamp=timestamp, source=get_repo_info(), findings=[])
     
-    findings = data['findings']
     rules = data['rules']
-
+    findings = data['findings']
     for finding in findings:
         rule_id = finding['rule_id']
         rule = rules[rule_id]
@@ -77,19 +74,18 @@ def get_poutine_results(timestamp: datetime) -> ResultsRequest | None:
 def get_frizbee_results(timestamp: datetime) -> ResultsRequest | None: 
     FRIZBEE_FILE = 'frizbee-out.jsonl'
 
-    results = []
-
+    json_lines = []
     try:
         with open(FRIZBEE_FILE, 'r') as f:
             for line in f:
-                results.append(json.loads(line))
+                json_lines.append(json.loads(line))
     except OSError:
         return None
     
     results_request = ResultsRequest(scanner="frizbee", timestamp=timestamp, source=get_repo_info(), findings=[])
     
-    for findings in results:
-        for finding in findings['findings']:
+    for json_line in json_lines:
+        for finding in json_line['findings']:
             title = 'Pin to commit SHA instead of tag'
             old = finding['old']
             new = finding['new']
@@ -104,8 +100,35 @@ def get_frizbee_results(timestamp: datetime) -> ResultsRequest | None:
     return results_request
     
 
-def get_semgrep_results(): 
+def get_semgrep_results(timestamp: datetime) -> ResultsRequest | None: 
     SEMGREP_FILE = 'semgrep-out.json'
+
+    try:
+        with open(SEMGREP_FILE, 'r') as f:
+            data = json.load(f)
+    except OSError:
+        return None
+    
+    results_request = ResultsRequest(scanner="semgrep", timestamp=timestamp, source=get_repo_info(), findings=[])
+
+    results = data['results']
+    for result in results:
+        file = result['path']
+        lineStart = result['start']['line']
+        lineEnd = result['end']['line']
+
+        extra = result['extra']
+        description = extra['message']
+        title = str(extra['metadata']['cwe'][0])
+
+        title = title.split(': ', 1)[-1]
+
+
+        results_request.findings.append(
+            Finding(title=title, description=description, file=file, lineStart=lineStart, lineEnd=lineEnd)
+        )
+    
+    return results_request
     
 
 def main():
@@ -122,14 +145,22 @@ def main():
     actionlint_results = get_actionlint_results(args.actionlint_timestamp)
     poutine_results = get_poutine_results(args.poutine_timestamp)
     frizbee_results = get_frizbee_results(args.frizbee_timestamp)
-    semgrep_results = get_semgrep_results()
+    semgrep_results = get_semgrep_results(args.semgrep_timestamp)
 
     if actionlint_results is None:
         print(f'{RED}ERROR: Failed to read actionlint results file{RESET}')
     if poutine_results is None:
         print(f'{RED}ERROR: Failed to read poutine results file{RESET}')
 
+    #TODO: remove print statements when the script sends the results to the SOAP service
+    print('------------------actionlint------------------')
+    print(actionlint_results)
+    print('-------------------poutine-------------------')
+    print(poutine_results)
+    print('-------------------frizbee-------------------')
     print(frizbee_results)
+    print('-------------------semgrep-------------------')
+    print(semgrep_results)
 
 
 if __name__ == '__main__':
